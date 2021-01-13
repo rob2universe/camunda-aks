059c50edaa4d8342dbac1bcbe276a6737c5ed8a1
rob2universe <robert.emsbach@gmail.com>
2021-01-13 18:45:03 +0800

    cleanup

--- createCluster.ps1
+++ createCluster.ps1
@@ -1,15 +1,21 @@
-az group create -n rgaks -l southeastasia
-az acr create -g rgaks -n robsacr --sku Basic
-az acr login -n robsacr
+$rgName = rgaks
+$acrName = robsacr
+$clusterName = camundaCluster
+az group create -n $rgName -l southeastasia
 
+# az acr create -g $rgName -n robsacr --sku Basic
+# az acr login -n robsacr
+# docker push robsacr.azurecr.io/azure-vote-front:v1
+# docker push robsacr.azurecr.io/camunda/camunda-bpm-platform:run-latest
 
-docker push robsacr.azurecr.io/azure-vote-front:v1
-docker push robsacr.azurecr.io/camunda/camunda-bpm-platform:run-latest
-
-az aks create -g rgaks -n camAKSCluster --node-count 2 --generate-ssh-keys --attach-acr robsacr
-az aks get-credentials -g rgaks -n camAKSCluster
+az aks create -g $rgName -n camundaCluster --node-count 2 --generate-ssh-keys --attach-acr $acrName
+az aks get-credentials -g $rgName -n camundaCluster
 
 kubectl get nodes
 
-kubectl apply -f .\azure-camunda-bpm-run.yaml
+# kubectl apply -f .\azure-camunda-bpm-run.yaml
+kubectl apply -f .\pod.yml
+kubectl apply -f .\service.yml
+kubectl apply -f .\ingress.yml
+
 kubectl get service camunda-bpm-run --watch
